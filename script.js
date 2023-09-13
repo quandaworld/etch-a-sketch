6684def51805d31a3ea610c80d712bfe9dfeb817
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-13 15:38:26 -0700

    Update fillGrid function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const container_div = document.querySelector('.grid-container');
 
-
-function fillGrid() {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+function fillGrid(squareNum = 16) {
+  for (let i = 0; i < squareNum; i++) {
+    for (let j = 0; j < squareNum; j++) {
       const square_div = document.createElement('div');
       square_div.classList.add('grid-square');
-      // square_div.textContent = '.';                       
+      square_div.style.cssText =`min-width: calc(1/${squareNum}*100%)`;
       container_div.appendChild(square_div);
     }
   }
