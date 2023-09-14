5e6520a0be273efcc2f77d41fcaffde0b1e09af5
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-13 18:22:41 -0700

    Add hover effect

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container_div = document.querySelector('.grid-container');
 
+function changeColor(e, color = 'blue') {
+  e.target.style.backgroundColor = color;
+  console.log(e);
+}
+
 function fillGrid(squareNum = 16) {
-  for (let i = 0; i < squareNum; i++) {
-    for (let j = 0; j < squareNum; j++) {
-      const square_div = document.createElement('div');
-      square_div.classList.add('grid-square');
-      square_div.style.cssText =`min-width: calc(1/${squareNum}*100%)`;
-      container_div.appendChild(square_div);
-    }
+  for (let i = 0; i < squareNum * squareNum; i++) {
+    const square_div = document.createElement('div');
+    square_div.classList.add('grid-square');
+    container_div.appendChild(square_div);
+    square_div.style.cssText =`min-width: calc(1 / ${squareNum} * 100%)`; // Maximum number of squares in a row at any time
+    square_div.addEventListener('mouseenter', changeColor);
   }
 }
 
