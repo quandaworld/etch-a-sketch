254206f7f2f1d9e2816688d3ca9cb71ad75d4473
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-14 11:20:46 -0700

    Update border class name

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function fillGrid(size = 16) {
   for (let i = 1; i <= size * size; i++) {
     const square_div = document.createElement('div');
     square_div.classList.add('grid-square');
-    if (i >= 1 && i <= size) square_div.classList.add('top');
-    if (i % size === 0) square_div.classList.add('right');
-    container_div.appendChild(square_div);
+    if (i >= 1 && i <= size) square_div.classList.add('border-top');
+    if (i % size === 0) square_div.classList.add('border-right');
     square_div.style.cssText =`min-width: calc(1 / ${size} * 100%)`; // Maximum number of squares in a row at any time
+    container_div.appendChild(square_div);
     square_div.addEventListener('mouseenter', changeColor);
   }
 }
