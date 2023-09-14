6476f25d75cf7fee61cd354b63f1b1de91cda6f9
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-14 11:15:15 -0700

    Update fillGrid to prevent duplicated borders

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function changeColor(e, color = 'blue') {
   console.log(e);
 }
 
-function fillGrid(squareNum = 16) {
-  for (let i = 0; i < squareNum * squareNum; i++) {
+function fillGrid(size = 16) {
+  for (let i = 1; i <= size * size; i++) {
     const square_div = document.createElement('div');
     square_div.classList.add('grid-square');
+    if (i >= 1 && i <= size) square_div.classList.add('top');
+    if (i % size === 0) square_div.classList.add('right');
     container_div.appendChild(square_div);
-    square_div.style.cssText =`min-width: calc(1 / ${squareNum} * 100%)`; // Maximum number of squares in a row at any time
+    square_div.style.cssText =`min-width: calc(1 / ${size} * 100%)`; // Maximum number of squares in a row at any time
     square_div.addEventListener('mouseenter', changeColor);
   }
 }
