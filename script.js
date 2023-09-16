f0ed74764aa590d384fd21fffe9d0901b9733444
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-15 22:09:35 -0700

    Update JS file

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+const DEFAULT_SIZE = 20;
+const DEFAULT_COLOR = '#3d3d3d';
 const container_div = document.querySelector('.grid-container');
 
-function changeColor(e, color = 'blue') {
-  e.target.style.backgroundColor = color;
-  console.log(e);
+
+function changeColor(square, color = DEFAULT_COLOR) {
+  square.style.backgroundColor = color;
 }
 
-function fillGrid(size = 16) {
+
+function fillGrid(size = DEFAULT_SIZE) {
   for (let i = 1; i <= size * size; i++) {
     const square_div = document.createElement('div');
     square_div.classList.add('grid-square');
     if (i >= 1 && i <= size) square_div.classList.add('border-top');
     if (i % size === 0) square_div.classList.add('border-right');
-    square_div.style.cssText =`min-width: calc(1 / ${size} * 100%)`; // Maximum number of squares in a row at any time
+    square_div.style.minWidth =`calc(1 / ${size} * 100%)`; // Maximum number of square_div in a row at any time
     container_div.appendChild(square_div);
-    square_div.addEventListener('mouseenter', changeColor);
+
+    const helper = () => changeColor(square_div);
+
+    container_div.addEventListener('click', () => {
+      square_div.addEventListener('mouseenter', helper);
+    });
+
+    container_div.addEventListener('dblclick', () => {
+      square_div.removeEventListener('mouseenter', helper);
+    });
   }
 }
 
 fillGrid();
 
 
-// Get current year in footer's copyright
-document.getElementById('year').textContent = new Date().getFullYear();
+
+
+document.getElementById('year').textContent = new Date().getFullYear(); // Get current year in footer's copyright
