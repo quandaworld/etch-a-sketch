e0238a510b797bab772abef1b47c9e0014042a68
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-18 14:46:21 -0700

    Add functions to eraser and reset buttons

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#3d3d3d';
 const container_div = document.querySelector('.grid-container');
+const color_button = document.getElementById('color');
+const rainbow_button = document.getElementById('rainbow');
+const eraser_button = document.getElementById('eraser');
+const reset_button = document.getElementById('reset');
+const size_span = document.getElementById('grid-size');
+const slider_input = document.getElementById('myRange');
 let isPressed = false;
+let eraser = false;
 
 function changeColor(e, color = DEFAULT_COLOR) {
-  if (isPressed) e.target.style.backgroundColor = color;
+  if (isPressed && eraser) e.target.style.backgroundColor = '';
+  if (isPressed && !eraser) e.target.style.backgroundColor = color;
+}
+
+function addMouseEvent(element) {
+  element.addEventListener('mousedown', () => isPressed = true);
+  document.body.addEventListener('mouseup', () => isPressed = false);
+  element.addEventListener('mousemove', changeColor);
 }
 
 function fillGrid(size = DEFAULT_SIZE) {
@@ -15,13 +29,25 @@ function fillGrid(size = DEFAULT_SIZE) {
     if (i % size === 0) square_div.classList.add('border-right');
     square_div.style.minWidth =`calc(1 / ${size} * 100%)`; // Maximum number of square_div in a row at any time
     container_div.appendChild(square_div);
-    square_div.addEventListener('mousedown', () => isPressed = true);
-    square_div.addEventListener('mouseup', () => isPressed = false);
-    square_div.addEventListener('mousemove', changeColor);
+    addMouseEvent(square_div);
   }
 }
 
 fillGrid();
+
+const gridSquare_list = document.querySelectorAll('.grid-square');
+
+eraser_button.addEventListener('click', () => {
+  eraser = true;
+  gridSquare_list.forEach(square => addMouseEvent(square));
+});
+
+reset_button.addEventListener('click', () => {
+  gridSquare_list.forEach(square => {
+    square.style.backgroundColor = '';
+  });
+  eraser = false;
+});
 
 
 
